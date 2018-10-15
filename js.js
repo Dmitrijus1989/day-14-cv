7b5e5fd749d2d713b96a55e5c618da2d9196bb6f
Dmitrijus <Ravus@mail.ru>
2018-10-15 15:16:29 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,61 @@
+var bar1 = new ProgressBar.Line(progressBar1, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+var bar2 = new ProgressBar.Line(progressBar2, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+var bar3 = new ProgressBar.Line(progressBar3, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+var bar4 = new ProgressBar.Line(progressBar4, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+var bar5 = new ProgressBar.Line(progressBar5, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+var bar6 = new ProgressBar.Line(progressBar6, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+
+bar1.animate(0.70);
+bar2.animate(0.85);
+bar3.animate(0.9);
+bar4.animate(0.8);
+bar5.animate(0.75);
+bar6.animate(0.5);
